5196154b933032861b2d18cada1bfbf40b99bb90
inobagger <inobagger@gmail.com>
2017-09-09 22:27:39 +0200

    Added Algorithm

---
+++ algorithm.py
@@ -0,0 +1,55 @@
+import urllib2
+import json
+import datetime
+import sys
+
+priceKm = 0.2
+priceHour = 12
+
+def algorithm(origin, destination, waypoints):
+	key = "AIzaSyDLpmbzdP0zZPJsb5LxN6QxEefuSorjRkg"
+
+	print "Processing route from %s to %s stopping at %s..." % (origin, destination, waypoints)
+	link = "https://maps.googleapis.com/maps/api/directions/json?origin="+origin+"&destination="+destination+"&key="+key
+	r = urllib2.urlopen(link.replace(" ","%20"))
+	a = json.loads(r.read())
+	#print "Ruta directa:"
+	distancia = a["routes"][0]["legs"][0]["distance"]["value"]
+	tiempo = a["routes"][0]["legs"][0]["duration"]["value"]
+	distanciaOriginal = distancia
+	tiempoOriginal = tiempo
+
+	if waypoints == "":
+		wayp = False
+		sys.exit(0)
+
+	numberOfWaypoints = len(waypoints.split("|"))
+	link = "https://maps.googleapis.com/maps/api/directions/json?origin="+origin+"&destination="+destination+"&waypoints="+	waypoints+"&key="+key
+	r = urllib2.urlopen(link.replace(" ","%20"))
+	a = json.loads(r.read())
+	#print ""
+	#print "Debug link: " + link
+	#print ""
+
+	totalKm = 0
+	totalTime = 0
+	for i in range(numberOfWaypoints+1):
+		#print "Ruta [%d/%d]" % (i+1, numberOfWaypoints+1)
+		#print "From: " + a["routes"][0]["legs"][i]["start_address"]
+		#print "To: " + a["routes"][0]["legs"][i]["end_address"]
+		distancia = a["routes"][0]["legs"][i]["distance"]["value"]
+		tiempo = a["routes"][0]["legs"][i]["duration"]["value"]
+		totalKm += distancia
+		totalTime += tiempo
+		#print str(distancia/1000) + " km"
+		#print str(datetime.timedelta(seconds=tiempo))
+		#print ""
+	timeDifference = totalTime - tiempoOriginal
+	distanceDifference = totalKm - distanciaOriginal
+	print "Distancia / tiempo en ruta original: " + str(distanciaOriginal/1000) + " km / " + str(datetime.timedelta(seconds=tiempoOriginal))
+	print "Distancia / tiempo en ruta desviada: " + str(totalKm/1000) + " km / " + str(datetime.timedelta(seconds=totalTime))
+	print "Diferencia de distancia / tiempo: " + str(distanceDifference/1000) + " km / " + str(datetime.timedelta(seconds=timeDifference))
+	ganancia = 6000 - (totalKm/1000) *priceKm - totalTime/3600.0*priceHour
+	print "Ganancia neta: "+ str(ganancia) + "E"
+	print ""
+	return ganancia
---
+++ main.py
@@ -0,0 +1,5 @@
+import algorithm
+
+g1 = algorithm.algorithm("Madrid", "Munich", "Paris|Frankfurt")
+g2 = algorithm.algorithm("Madrid", "Munich", "Lyon|Zurich")
+g3 = algorithm.algorithm("Madrid", "Munich", "Barcelona|Milan")
